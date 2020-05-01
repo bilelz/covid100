8d3fb1b80367ccdf1c8defa9ec5bbadcee7c772f
ZEGHAD <extern.bilel.zeghad@europcar.com>
2020-05-01 13:36:23 +0200

    fit bound

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-var initLatlng = [46.911637, 2.724609];
+var initLatlng = { lat: 46.911637, lng: 2.724609 },
+  circle100 = undefined;
 
-var mymap = L.map("map").setView(initLatlng, 5);
+var map = L.map("map").setView(initLatlng, 5);
 
 L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
   {
     maxZoom: 18,
+
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -14,29 +16,72 @@ L.tileLayer(
     tileSize: 512,
     zoomOffset: -1,
   }
-).addTo(mymap);
+).addTo(map);
+map.zoomControl.setPosition("bottomright");
 
-// L.marker(initLatlng).addTo(mymap).bindPopup("<b>Hello!</b><br />Cliquez n'importe ou sur la carte pour afficher votre zone de 100km").openPopup();
-
-drawCircle({ latlng: initLatlng }, "<b>Hello!</b><br />Cliquez n'importe ou sur la carte pour afficher votre zone de 100km");
-
-function onMapClick(e) {
-  drawCircle(e);
+function france() {
+  drawCircle({ latlng: initLatlng }, "Cliquez n'importe ou sur la carte pour afficher votre zone de 100km", false);
+  map.setView(initLatlng, 5);
 }
 
-mymap.on("click", onMapClick);
+france();
 
-function drawCircle(e, msg) {
-  if (msg) {
-    var popup = L.popup();
-    popup.setLatLng(e.latlng).setContent(msg).openOn(mymap);
+function onMapClick(e) {
+  drawCircle(e, null, true);
+}
+
+map.on("click", onMapClick);
+
+function drawCircle(e, msg, fit) {
+  if (circle100 !== undefined) {
+    map.removeLayer(circle100);
   }
 
-  L.circle(e.latlng, 100 * 1000, {
-    color: "green",
-    fillColor: "#f03",
-    fillOpacity: 0.1,
-  })
-    .addTo(mymap)
-    .bindPopup("I am a circle.");
+  circle100 = L.circle(e.latlng, 100 * 1000, {
+    color: "white",
+    fillColor: "white",
+    fillOpacity: 0.5,
+  }).addTo(map);
+
+  if (msg) {
+    var popup = L.popup();
+    popup.setLatLng(e.latlng).setContent(msg).openOn(map);
+  }
+
+  map.panTo(new L.LatLng(e.latlng.lat, e.latlng.lng), { duration: 1 });
+
+  if (fit) {
+    map.fitBounds(circle100.getBounds(), { padding: [10, 10] });
+  }
+}
+
+function gps() {
+  if ("geolocation" in navigator) {
+    document.getElementById("gpsStatus").innerText = "⌛...";
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        drawCircle({ latlng: { lat: position.coords.latitude, lng: position.coords.longitude } }, "🏠 Autour de chez vous", true);
+        document.getElementById("gpsStatus").innerText = "";
+      },
+      function (error) {
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            document.getElementById("gpsStatus").innerText = "Vous avez refusé la demande de localisation";
+            break;
+          case error.POSITION_UNAVAILABLE:
+            document.getElementById("gpsStatus").innerText = "Information localisation indisponible";
+            break;
+          case error.TIMEOUT:
+            document.getElementById("gpsStatus").innerText = "Temps d'attente trop long";
+            break;
+          case error.UNKNOWN_ERROR:
+            document.getElementById("gpsStatus").innerText = "Bouh... erreur inconnue";
+            break;
+        }
+      }
+    );
+  } else {
+    /* la géolocalisation n'est pas disponible */
+    document.getElementById("gps").innerHTML = "<p>Votre appareil ne permet la geo-localisation, essayez depuis un autre</p>";
+  }
 }
