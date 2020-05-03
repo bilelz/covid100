783c59ed518becd4ab95b42a36c2cbb35a7d6fea
ZEGHAD <extern.bilel.zeghad@europcar.com>
2020-05-03 14:00:53 +0200

    new logo 💯

--- js/main.js
+++ js/main.js
@@ -80,7 +80,9 @@ function drawCircle(e, msg, fit) {
 function france() {
   var msg =
     "Cliquez n'importe où sur la carte pour afficher une zone de 100 km <br/> " +
-    '<span data-gps> ou <button type="button" onclick="gps()">cliquez ici</button> pour être géo-localisé.</span>';
+    '<button type="button" onclick="gps()" class="invert" data-gps>' +
+    '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"> <path d="M0 0h24v24H0z" fill="none" /> <path      d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/></svg>' +
+    "&nbsp;Cliquez ici pour être géo-localisé.</button>";
   setRadius(100 * 1000);
   drawCircle({ latlng: initLatlng }, msg, false);
   map.setView(initLatlng, 5);
