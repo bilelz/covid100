0ce6009f2c03a7c09e98164c750ab4337c098868
ZEGHAD <extern.bilel.zeghad@europcar.com>
2020-05-18 13:07:21 +0200

    stolmen

--- js/main.js
+++ js/main.js
@@ -40,30 +40,6 @@ const OpenStreetMap_France = L.tileLayer("https://{s}.tile.openstreetmap.fr/osmf
   attribution: '&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 });
 
-const OpenTopoMap = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
-  maxZoom: 17,
-  attribution:
-    'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-});
-
-const Stadia_AlidadeSmooth = L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png", {
-  maxZoom: 20,
-  attribution:
-    '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-});
-
-const Stadia_AlidadeSmoothDark = L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png", {
-  maxZoom: 20,
-  attribution:
-    '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-});
-
-const Stadia_OSMBright = L.tileLayer("https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png", {
-  maxZoom: 20,
-  attribution:
-    '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-});
-
 const Stamen_Watercolor = L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}", {
   attribution:
     'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -81,16 +57,21 @@ const map = L.map("map", {
 
 const baseLayers = {
   "Classic (OSM)": OpenStreetMap_France,
-  OpenTopoMap: OpenTopoMap,
-  "Stadia Bright": Stadia_OSMBright,
-  Stadia: Stadia_AlidadeSmooth,
-  "Stadia sombre/dark ": Stadia_AlidadeSmoothDark,
   "📜 Parchemin": Stamen_Watercolor,
 };
 
 L.control.layers(baseLayers, null, { position: "bottomright" }).addTo(map);
 
 map.zoomControl.setPosition("bottomright");
+
+let currentMapIndex = 0;
+document.querySelector("body").setAttribute("data-map", currentMapIndex);
+function toggleMap() {
+  currentMapIndex = currentMapIndex === 1 ? 0 : 1;
+  var layerControlElement = document.querySelector(".leaflet-control-layers");
+  layerControlElement.getElementsByTagName("input")[currentMapIndex].click();
+  document.querySelector("body").setAttribute("data-map", currentMapIndex);
+}
 
 function setRadius(d) {
   if (circle100) {
